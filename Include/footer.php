82baf026daeed6203cb23f4c8a23adaef6d3d72c
T.V. Chathuni Janithya <janithyatvc@gmail.com>
2026-09-04 22:31:47 +0530

    Create file structure for the Online Food Ordering System

---
+++ Include/footer.php
@@ -0,0 +1,7 @@
+</div>
+
+    <div class="footer">
+        <p>&copy; 2024 Online Food Ordering System. All rights reserved.</p>
+    </div>
+</body>
+</html>
---
+++ site/index.php
@@ -0,0 +1,8 @@
+<?php
+    include("../Include/header.php");
+?>
+        <h1>Welcome to Our Online Food Ordering System</h1>
+        <p>Order your favorite meals online and have them delivered to your doorstep!</p>
+<?php
+    include("../Include/footer.php");       
+?>
